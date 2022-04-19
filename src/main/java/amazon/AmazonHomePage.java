7073005e5b5599d3fede1756bfc129121d19b590
ZK0011 <zartasha067@gmail.com>
2022-04-19 00:49:07 -0400

    Update

--- src/main/java/amazon/AmazonHomePage.java
+++ src/main/java/amazon/AmazonHomePage.java
@@ -1,7 +1,14 @@
 package amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class AmazonHomePage extends BasePage {
+    private static final String URL = "https://www.amazon.com/";
+
+       // @FindBy(xpath =
 
 }
