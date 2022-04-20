82d4a047af07ce9cf46ad8b94bbe4f2435f5d417
FrancescaPasse <fp2jf@virgini.edu>
2022-04-20 12:20:36 -0400

    Finding 5 RegistryPage xpaths

--- src/main/java/amazon/RegistryPage.java
+++ src/main/java/amazon/RegistryPage.java
@@ -1,6 +1,48 @@
 package amazon;
 
 import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class RegistryPage extends BasePage {
+    static final String URL=" https://www.amazon.com/registries?ref_=nav_cs_registry";
+
+    @FindBy (xpath = "(//input[@placeholder='Search by Registrant name'])")
+    public WebElement registryNameSearchBox;
+
+    @FindBy (xpath = "(//span[@class='a-dropdown-prompt'])")
+    public WebElement registryListDropdownBox;
+
+    @FindBy (xpath = "(//button[@class='gr-btn gr-btn--rounded gr-text gr-find-stripe__cta gr-find-stripe__submit'])")
+    public WebElement registrySearchButton;
+
+    @FindBy (xpath = "(//div[class='gr-registry-types__header'])")
+    public WebElement registryWeddingSearchButton;
+
+    @FindBy (xpath = "(//img[@alt='Couple Kissing'])")
+    public WebElement registryWeddingImageField;
+
+    public RegistryPage() {
+        webDriverWait.until(ExpectedConditions.urlToBe(URL));
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
