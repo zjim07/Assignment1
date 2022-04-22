7ee10e3f0cb6e6c2f2c96c682da05d163c0f0bf7
manahilmalik15 <manahilmalik15@hotmail.com>
2022-04-22 18:09:59 -0400

    updated test cases

--- src/main/java/amazon/AmazonBasicsPage.java
+++ src/main/java/amazon/AmazonBasicsPage.java
@@ -9,16 +9,16 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class AmazonBasicsPage extends BasePage {
     private static final String URL = "https://www.amazon.com";
 
-    @FindBy(xpath = "//*[@id='k9rs2mhe3v']/div/div/div/a")
+    @FindBy(xpath ="(//a[@class='style__overlay__2qYgu EditorialTile__overlay__3cghg'])[1]" )
     public WebElement newArrivals;
-    @FindBy(xpath = "//*[@id=\'3uu6f2odnn\']/div/div/div/a")
-    public WebElement lightningCables;
-    @FindBy(xpath = "//*[@id=\'reaapr152d\']/div/div/div/a")
-    public WebElement workFromHome;
-    @FindBy(xpath="//*[@id="krx4r15dsa"]/div/div/div/a")
-    public WebElement automotiveSupplies;
-    @FindBy(xpath="//*[@id=\"td2ouz0m0o\"]/div/div/div/a")
-    public WebElement fitness;
+    //@FindBy(xpath = "//*[@id=\'3uu6f2odnn\']/div/div/div/a")
+    //public WebElement lightningCables;
+    //@FindBy(xpath = "//*[@id=\'reaapr152d\']/div/div/div/a")
+   // public WebElement workFromHome;
+    //@FindBy(xpath="//*[@id="krx4r15dsa"]/div/div/div/a")
+   // public WebElement automotiveSupplies;
+    //@FindBy(xpath="//*[@id=\"td2ouz0m0o\"]/div/div/div/a")
+    //public WebElement fitness;
 
     public AmazonBasicsPage() {
         webDriverWait.until(ExpectedConditions.urlContains(URL));
--- src/test/java/amazon/TestAmazonBasicsPage.java
+++ src/test/java/amazon/TestAmazonBasicsPage.java
@@ -7,31 +7,31 @@ public class TestAmazonBasicsPage extends TestBasePage {
     @Test
     public void testNavigateToNewArrivals(){
         Homepage homepage=new Homepage();
-        AmazonBasicsPage amazonBasicsPage=Homepage.navigateToBasicsPage();
+        AmazonBasicsPage amazonBasicsPage=homepage.navigateToAmazonBasicsPage();
         amazonBasicsPage.newArrivals.click();
     }
     @Test
     public void testNavigateToLightningCables() {
         Homepage homepage = new Homepage();
-        AmazonBasicsPage amazonBasicsPage = Homepage.navigateToBasicsPage();
+        AmazonBasicsPage amazonBasicsPage = homepage.navigateToAmazonBasicsPage();
         amazonBasicsPage.lightningCables.click();
     }
         @Test
-                public void testNavigateToWorkFromHome(){
-            Homepage homepage=new Homepage();
-            AmazonBasicsPage BasicsPage=Homepage.navigatetoBasicsPage();
+                public void testNavigateToWorkFromHome() {
+            Homepage homepage = new Homepage();
+            AmazonBasicsPage amazonBasicsPage = homepage.navigateToAmazonBasicsPage();
             amazonBasicsPage.workFromHome.click();
-
+        }
             @Test
-                    public voidtestNavigateToAutomotiveSupplies(){
-                Homepage homepage=new Homepage();
-                AmazonBasicsPage BasicsPage=Homepage.navigatetoBasicsPage();
+                    public void testNavigateToAutomotiveSupplies(){
+                Homepage homepage = new Homepage();
+                AmazonBasicsPage amazonBasicsPage = homepage.navigateToAmazonBasicsPage();
                 amazonBasicsPage.automotiveSupplies.click();
-
+            }
                 @Test
-                        public voidtestNavigateToFitness(){
+                        public void testNavigateToFitness(){
                     Homepage homepage=new Homepage();
-                    AmazonBasicsPage BasicsPage=Homepage.navigatetoBasicsPage();
+                    AmazonBasicsPage amazonBasicsPage=homepage.navigateToAmazonBasicsPage();
                     amazonBasicsPage.fitness.click();
                 }
             }
