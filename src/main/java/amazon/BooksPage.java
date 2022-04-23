585b4c7b3ecace1381857cc29207fa0e45523cef
samarali27 <samalimoh94@gmail.com>
2022-04-22 23:40:23 -0400

    BookPage

--- src/main/java/amazon/BooksPage.java
+++ src/main/java/amazon/BooksPage.java
@@ -13,17 +13,24 @@ public class BooksPage extends BasePage {
     @FindBy(xpath = "//a[@href='/books-used-books-textbooks/b/?ie=UTF8&node=283155&ref_=nav_cs_books']")
     public WebElement mainBookImage;
 
-    public BooksPage(){
+    public BooksPage() {
         webDriverWait.until(ExpectedConditions.urlContains(URL));
         PageFactory.initElements(driver, this);
-        
+
     }
+
     @FindBy(xpath = "nav-search-submit-button")
     public WebElement SearchElement;
 
- // public SearchElement(){
-   //   webDriverWait.until(ExpectedConditions.urlContains(URL));
-     // PageFactory.initElements(driver, this);
 
-  }
+    @FindBy(xpath = "//*[@id='s-refinements/div[3]/ul/li[17]/span/a/span']")
+    public WebElement lawBooks;
 
+    @FindBy(xpath = "//*[@id='s-refinements/div[3]/ul/li[6]/span/a/span']")
+    public WebElement ChildrensBooks;
+
+    @FindBy(xpath = "//*[@id='s-refinements/div[3]/ul/li[15]/span/a/span']")
+    public WebElement HistoryBooks;
+
+
+}
--- src/test/java/amazon/TestBooksPage.java
+++ src/test/java/amazon/TestBooksPage.java
@@ -3,6 +3,7 @@ package amazon;
 import base.TestBasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertNotNull;
@@ -13,18 +14,59 @@ public class TestBooksPage extends TestBasePage {
     private Object SearchResults;
 
     @Test
-    public void TestBookPage() {
+    public void testNavigateToTestBookPage() {
         Homepage homepage = new Homepage();
         BooksPage BookPage = homepage.navigateToBooksPage();
         BookPage.mainBookImage.click();
     }
-        @Test
-        public void TestSearchBook() {
-            WebElement SearchElement = driver.findElement(By.id("nav-search-submit-button"));
-            SearchElement.sendKeys("best sellers");
-            driver.findElement(By.className("nav-input")).click();
-           // WebElement SearchResults = driver.findElement(By.linkText("bast seller books"));
 
-            assertNotNull(SearchResults);
-        }
+    @Test
+    public void testNavigateToTestSearchBook() {
+        WebElement SearchElement = driver.findElement(By.id("nav-search-submit-button"));
+        SearchElement.sendKeys("best sellers");
+        driver.findElement(By.className("nav-input")).click();
+        // WebElement SearchResults = driver.findElement(By.linkText("bast seller books"));
+
+        assertNotNull(SearchResults);
     }
+
+    @Test
+    public void testNavigateToTestLowBook() {
+        Homepage homepage = new Homepage();
+        BooksPage BookPage = homepage.navigateToBooksPage();
+        BookPage.lawBooks.click();
+
+        String expectedText = "//*[@id='s-refinements/div[3]/ul/li[17]/span/a/span']";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText,text);
+
+
+    }
+
+
+    @Test
+    public void TestChildrensBooks() {
+        Homepage homepage = new Homepage();
+        BooksPage BookPage = homepage.navigateToBooksPage();
+        BookPage.ChildrensBooks.click();
+
+
+        String expectedText = "//*[@id='s-refinements/div[3]/ul/li[6]/span/a/span']";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText,text);
+    }
+
+    @Test
+    public void TestHistoryBooks() {
+        Homepage homepage = new Homepage();
+        BooksPage BookPage = homepage.navigateToBooksPage();
+        BookPage.HistoryBooks.click();
+
+        String expectedText = "";
+        String text = driver.getTitle();
+        System.out.println(text);
+        Assert.assertEquals(expectedText,text);
+    }
+}
